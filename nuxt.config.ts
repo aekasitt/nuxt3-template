d7ef3aab0bc8a1efa55ec1209f9828702fadab2d
Sitt Guruvanich <aekazitt@gmail.com>
2022-09-06 13:49:28 +0700

    Revise additional tailwind and colorMode configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  colorMode: {
+    classSuffix: ''
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.ts',
+    exposeConfig: false,
+    config: {},
+    injectPosition: 0,
+    viewer: true,
+  },
   ssr: false,
 })
